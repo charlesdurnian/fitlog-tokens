67759587ad12cf5ba1082f341889fced78d219a9
Charles Durnian <charlesdurnian@gmail.com>
2025-04-25 10:02:12 +0100

    Initial commit

---
+++ style-dictionary.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+    source: ["tokens/**/*.json"],
+    platforms: {
+      web: {
+        transformGroup: "web",
+        buildPath: "dist/web/",
+        files: [{
+          destination: "tailwind.tokens.js",
+          format: "javascript/es6"
+        }]
+      },
+      ios: {
+        transformGroup: "ios",
+        buildPath: "dist/ios/",
+        files: [{
+          destination: "StyleDictionary.swift",
+          format: "ios-swift/class.swift",
+          className: "StyleDictionary"
+        }]
+      },
+      android: {
+        transformGroup: "android",
+        buildPath: "dist/android/",
+        files: [{
+          destination: "tokens.xml",
+          format: "android/xml"
+        }]
+      }
+    }
+  }
+  
